e615469706fee48106f409fbf3eef802cd602950
tcharding <me@tobin.cc>
2019-05-30 17:14:28 +1000

    rust: Add minigrep implementation
    Signed-off-by: tcharding <me@tobin.cc>

---
+++ rust/rust-book/minigrep/src/lib.rs
@@ -0,0 +1,133 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a filename"),
+        };
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Taken from rustbook project 'adder'
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    // Taken from rustbook project 'adder'
+    #[test]
+    #[should_panic]
+    fn it_panics() {
+        panic!("this is supposed to fail");
+    }
+
+    #[test]
+    fn it_handles_correct_args_format() {
+        let binary = String::from("minigrep");
+        let query = String::from("needle");
+        let filename = String::from("haystack");
+        let args = [binary, query, filename];
+
+        let config = Config::new(&args).expect("test failed");
+        assert_eq!(config.query, "needle");
+        assert_eq!(config.filename, "haystack");
+    }
+
+    #[test]
+    fn it_handles_missing_args() {
+        let query = String::from("needle");
+        let args = [query];
+
+        let _config = match Config::new(&args) {
+            Ok(_) => panic!("should fail"),
+            Err(_) => return,
+        };
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
