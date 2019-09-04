21795f6468f59f2a0bd3265cb9b8d5eef71f4fcf
tcharding <me@tobin.cc>
2019-09-04 18:31:04 +1000

    rust: Add log transactions

--- rust/data-structure-book/transaction-log/src/main.rs
+++ rust/data-structure-book/transaction-log/src/main.rs
@@ -1,10 +1,9 @@
 use transaction_log::TransactionLog;
 
 fn main() {
-    println!("Transaction log, linked list example");
-
     let mut log = TransactionLog::new();
-    log.append(String::from("first log entry"));
+    log.append(String::from("first"));
+    log.append(String::from("second"));
 
     println!("{:?}", log);
 }
