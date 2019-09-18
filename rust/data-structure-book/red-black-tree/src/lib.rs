ad146fd2425f61fc4e36f9b4d55a76732bdc91e6
tcharding <me@tobin.cc>
2019-09-18 19:32:23 +1000

    rust: Add check()

--- rust/data-structure-book/red-black-tree/src/lib.rs
+++ rust/data-structure-book/red-black-tree/src/lib.rs
@@ -1,10 +1,11 @@
 use std::cell::RefCell;
+use std::mem;
 use std::rc::Rc;
 
 /// Enum to ease operations within the tree, LeftNode goes down the tree
 /// to the left and RightNode traverses down the tree to the right.
 #[derive(Debug)]
-enum RbOperation {
+pub enum RbOperation {
     LeftNode,
     RightNode,
 }
@@ -38,14 +39,6 @@ impl Node {
     }
 }
 
-/// All nodes to the left of a node will have less than or equal value.
-fn check(node: &BareTree, value: usize) -> RbOperation {
-    if value < node.borrow().value {
-        return RbOperation::LeftNode;
-    }
-    RbOperation::RightNode
-}
-
 /// Red Black Tree.
 pub struct Rbt {
     root: Tree,
@@ -68,101 +61,110 @@ impl Rbt {
         red_red == 0 && black_height_min == black_height_max
     }
 
-    // pub fn add(&mut self, value: usize) {
-    //     self.length += 1;
-    //     let root = mem::replace(&mut self.root, None);
-    //     let new_tree = self.add_r(root, value);
-    //     //        self.root = self.fix_tree(new_tree.1);
-    // }
+    pub fn add(&mut self, value: usize) {
+        self.length += 1;
+        let root = mem::replace(&mut self.root, None);
+        let new_tree = self.add_r(root, value);
+        self.root = self.fix_tree(new_tree.1);
+    }
 
-    // fn add_r(&mut self, mut node: Tree, value: usize) -> (Tree, BareTree) {
-    //     if let Some(n) = node.take() {
-    //         let new: BareTree;
+    fn add_r(&mut self, mut node: Tree, add_value: usize) -> (Tree, BareTree) {
+        if let Some(n) = node.take() {
+            let new: BareTree;
+            let node_value = n.borrow().value;
 
-    //         match n.check(value) {
-    //             RbOperation::LeftNode => {}
-    //             RbOperation::RightNode => {}
-    //         }
-    //         if value <= n.borrow().value {
-    //             let left = n.borrow().left.clone();
-    //             let new_tree = self.add_r(left, value);
-    //             new = new_tree.1;
-    //             let new_tree = new_tree.0.unwrap();
-    //             new_tree.borrow_mut().parent = Some(n.clone());
-    //             n.borrow_mut().left = Some(new_tree);
-    //         } else {
-    //             let right = n.borrow().right.clone();
-    //             let new_tree = self.add_r(right, value);
-    //             new = new_tree.1;
-    //             let new_tree = new_tree.0.unwrap();
-    //             new_tree.borrow_mut().parent = Some(n.clone());
-    //             n.borrow_mut().right = Some(new_tree);
-    //         }
-    //         (Some(n), new)
-    //     } else {
-    //         let new = Node::new(value);
-    //         (new.clone(), new.unwrap())
-    //     }
-    // }
+            match self.check(add_value, node_value) {
+                RbOperation::LeftNode => {
+                    let left = n.borrow().left.clone();
+                    let new_tree = self.add_r(left, add_value);
+                    new = new_tree.1;
+                    let new_tree = new_tree.0.unwrap();
+                    new_tree.borrow_mut().parent = Some(n.clone());
+                    n.borrow_mut().left = Some(new_tree);
+                }
+                RbOperation::RightNode => {
+                    let right = n.borrow().right.clone();
+                    let new_tree = self.add_r(right, add_value);
+                    new = new_tree.1;
+                    let new_tree = new_tree.0.unwrap();
+                    new_tree.borrow_mut().parent = Some(n.clone());
+                    n.borrow_mut().right = Some(new_tree);
+                }
+            }
+            (Some(n), new)
+        } else {
+            let new = Node::new(add_value);
+            (new.clone(), new.unwrap())
+        }
+    }
+
+    /// All nodes to the left of a node will have less than or equal value.
+    /// Checks whether add_value should be to the left or right of node_value.
+    fn check(&self, add_value: usize, node_value: usize) -> RbOperation {
+        if add_value <= node_value {
+            return RbOperation::LeftNode;
+        }
+        RbOperation::RightNode
+    }
+
+    fn fix_tree(&mut self, inserted: BareTree) -> Tree {
+        let mut not_root = inserted.borrow().parent.is_some();
+
+        let root = if not_root {
+            let mut parent_is_red = self.parent_color(&inserted) == Color::Red;
+            let mut n = inserted.clone();
+            while parent_is_red && not_root {
+                if let Some(uncle) = self.uncle(n.clone()) {
+                    let which = uncle.1;
+                    let uncle = uncle.0;
+
+                    match which {
+                        RbOperation::LeftNode {
+                            let mut parent = n.borrow().parent
+                                .as_ref().unwrap().clone();
+                            if uncle.is_some() && uncle.as_ref().unwrap().borrow().color == Color::Red {
+                                let uncle = uncle.unwrap();
+                                parent.borrow_mut().color = Color::Black;
+                                uncle.borrow_mut().color = Color::Black;
+                                parent.borrow().parent.as_ref().unwrap().borrow_mut().color = Color::Red;
+
+                                n = parent.borrow().parent.as_ref().unwrap().clone();
+                            } else {
+                                // do only if its the right child
+                                if parent.borrow().value > n.borrow().value {
+                                    let tmp = n.borrow().parent.as_ref().unwrap().clone();
+                                    n = tmp;
+                                    self.rotate_right(n.clone());
+                                    parent = n.borrow().parent.as_ref().unwrap().clone();
+                                }
+                                // until here then for all black uncles.
+                                parent.borrow_mut().color = Color::Black;
+                                parent.borrow().parent.as_ref().unwrap().borrow_mut().color = Color::Red;
+                                let grandparent = n.borrow().parent.as_ref().unwrap().borrow().parent.as_ref().unwrap().clone();
+                                self.rotate_left(grandparent)
+                            }
+                        }
+                    }
+                }
+                not_root = n.borrow().parent.is_some();
+                if not_root {
+                    parent_is_red = self.parent_color(&n) == Color::Red;
+                }
+            }
+            while n.borrow().parent.is_some() {
+                let t = n.borrow().parent.as_ref()unwrap().clone();
+                n = t;
+            }
+            Some(n)
+        } else {
+            Some(inserted)
+        };
+        root.map(|r| {
+            r.borrow_mut().color = Color::Black;
+            r
+        })
+    }
 }
-//     fn fix_tree(&mut self, inserted: BareTree) -> Tree {
-//         let mut not_root = inserted.borrow().parent.is_some();
-
-//         let root = if not_root {
-//             let mut parent_is_red = self.parent_color(&inserted) == Color::Red;
-//             let mut n = inserted.clone();
-//             while parent_is_red && not_root {
-//                 if let Some(uncle) = self.uncle(n.clone()) {
-//                     let which = uncle.1;
-//                     let uncle = uncle.0;
-
-//                     match which {
-//                         RbOperation::LeftNode {
-//                             let mut parent = n.borrow().parent
-//                                 .as_ref().unwrap().clone();
-//                             if uncle.is_some() && uncle.as_ref().unwrap().borrow().color == Color::Red {
-//                                 let uncle = uncle.unwrap();
-//                                 parent.borrow_mut().color = Color::Black;
-//                                 uncle.borrow_mut().color = Color::Black;
-//                                 parent.borrow().parent.as_ref().unwrap().borrow_mut().color = Color::Red;
-
-//                                 n = parent.borrow().parent.as_ref().unwrap().clone();
-//                             } else {
-//                                 // do only if its the right child
-//                                 if parent.borrow().value > n.borrow().value {
-//                                     let tmp = n.borrow().parent.as_ref().unwrap().clone();
-//                                     n = tmp;
-//                                     self.rotate_right(n.clone());
-//                                     parent = n.borrow().parent.as_ref().unwrap().clone();
-//                                 }
-//                                 // until here then for all black uncles.
-//                                 parent.borrow_mut().color = Color::Black;
-//                                 parent.borrow().parent.as_ref().unwrap().borrow_mut().color = Color::Red;
-//                                 let grandparent = n.borrow().parent.as_ref().unwrap().borrow().parent.as_ref().unwrap().clone();
-//                                 self.rotate_left(grandparent)
-//                             }
-//                         }
-//                     }
-//                 }
-//                 not_root = n.borrow().parent.is_some();
-//                 if not_root {
-//                     parent_is_red = self.parent_color(&n) == Color::Red;
-//                 }
-//             }
-//             while n.borrow().parent.is_some() {
-//                 let t = n.borrow().parent.as_ref()unwrap().clone();
-//                 n = t;
-//             }
-//             Some(n)
-//         } else {
-//             Some(inserted)
-//         };
-//         root.map(|r| {
-//             r.borrow_mut().color = Color::Black;
-//             r
-//         })
-//     }
-
 //     /// Returns true if value exists in the tree.
 //     pub fn find(&self, value: usize) -> bool {
 //         self.find_r(&self.root, value)
@@ -247,10 +249,25 @@ mod tests {
 
     #[test]
     fn check_works() {
-        let n = Node::new(6);
-        let bare_tree = n.take();
+        let rbt = Rbt::new();
 
-        assert_eq!(check(bare_tree, 5), RbOperation::LeftNode);
-        assert_eq!(check(bare_tree, 7), RbOperation::RightNode);
+        if let RbOperation::RightNode = rbt.check(5, 6) {
+            panic!("Expected LeftNode");
+        }
+
+        if let RbOperation::RightNode = rbt.check(6, 6) {
+            panic!("Expected LeftNode");
+        }
+
+        if let RbOperation::LeftNode = rbt.check(7, 6) {
+            panic!("Expected RightNode");
+        }
+    }
+
+    #[test]
+    fn can_add_value_to_tree() {
+        let mut rbt = Rbt::new();
+        rbt.add(5);
+        assert_eq!(rbt.length, 1);
     }
 }
